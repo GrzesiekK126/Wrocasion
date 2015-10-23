ad1913dec7e2e872df20a690723d1fe421be6ac6
GrzesiekK126 <grzesiek.knysz@gmail.com>
2015-10-23 23:16:58 +0200

    Logowanie przez Facebook'a

--- Wrocasion/app/src/main/java/app/wrocasion/MainActivity.java
+++ Wrocasion/app/src/main/java/app/wrocasion/MainActivity.java
@@ -1,127 +1,55 @@
 package app.wrocasion;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
+import android.widget.TextView;
+
 import android.view.View.OnClickListener;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
-    //Defining Variables
-    private Toolbar toolbar;
-    private NavigationView navigationView;
-    private DrawerLayout drawerLayout;
-    private Button button;
+    private TextView txtSkip;
+    static LoginButton loginButton;
+    static CallbackManager callbackManager;
+    static AccessTokenTracker accessTokenTracker;
+    static ProfileTracker profileTracker;
+    static boolean loginFacebook = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.start_activity);
 
-        // Initializing Toolbar and setting it as the actionbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        txtSkip = (TextView) findViewById(R.id.textView2);
+        txtSkip.setOnClickListener(this);
 
-        //Initializing NavigationView
-        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        loginToFacebook();
 
-        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    }
 
-            // This method will trigger on item Click of navigation menu
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-
-                //Checking if the item is in checked state or not, if not make it in checked state
-                if(menuItem.isChecked()) menuItem.setChecked(false);
-                else menuItem.setChecked(true);
-
-                //Closing drawer on item click
-                drawerLayout.closeDrawers();
-
-                //Check to see which item was being clicked and perform appropriate action
-                switch (menuItem.getItemId()){
-
-                    case R.id.first:
-                        Toast.makeText(getApplicationContext(),"First Selected",Toast.LENGTH_SHORT).show();
-                        FirstItemFragment firstFragment = new FirstItemFragment();
-                        FragmentTransaction firstFragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        firstFragmentTransaction.replace(R.id.frame,firstFragment);
-                        firstFragmentTransaction.commit();
-                        return true;
-
-                    // For rest of the options we just show a toast on click
-
-                    case R.id.second:
-                        Toast.makeText(getApplicationContext(),"Second Selected",Toast.LENGTH_SHORT).show();
-                        SecondItemFragment secondFragment = new SecondItemFragment();
-                        FragmentTransaction secondFragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        secondFragmentTransaction.replace(R.id.frame,secondFragment);
-                        secondFragmentTransaction.commit();
-                        return true;
-
-                    case R.id.third:
-                        Toast.makeText(getApplicationContext(),"Third Selected",Toast.LENGTH_SHORT).show();
-                        ThirdItemFragment thirdFragment = new ThirdItemFragment();
-                        FragmentTransaction thirdFragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        thirdFragmentTransaction.replace(R.id.frame,thirdFragment);
-                        thirdFragmentTransaction.commit();
-                        return true;
-
-                    case R.id.categories:
-                        Toast.makeText(getApplicationContext(),"Categories Selected",Toast.LENGTH_SHORT).show();
-                        Categories categories = new Categories();
-                        FragmentTransaction categoriesFragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        categoriesFragmentTransaction.replace(R.id.frame,categories);
-                        categoriesFragmentTransaction.commit();
-                        return true;
-                    default:
-                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
-                        return true;
-
-                }
-            }
-        });
-
-        // Initializing Drawer Layout and ActionBarToggle
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
-
-                super.onDrawerOpened(drawerView);
-            }
-        };
-
-        //Setting the actionbarToggle to drawer layout
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
-        //calling sync state is necessay or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -139,18 +67,92 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
-        }*/
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+
+    static String getName(Profile profile){
+        String name;
+        if(profile!=null) {
+            name = profile.getName();
+        }
+        else {
+            name = null;
+        }
+        return name;
+    }
+
+    static String getId(Profile profile){
+        String id;
+        if(profile!=null) {
+            id = profile.getId();
+        }
+        else {
+            id = null;
+        }
+        return id;
+    }
+
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.button) {
-            Intent intent = new Intent(this, Tabs.class);
-            startActivity(intent);
+       if(v.getId()==R.id.textView2){
+           Intent intent = new Intent(this, FirstActivity.class);
+           startActivity(intent);
+
         }
+        /*else if(v.getId()==R.id.login_button){
+           if(loginFacebook) {
+               Intent intent = new Intent(this, FirstActivity.class);
+               startActivity(intent);
+           }
+       }*/
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        getName(profile);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    static void loginToFacebook(){
+        callbackManager = CallbackManager.Factory.create();
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+
+
+            }
+        };
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+
+            }
+        };
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+        loginFacebook = true;
+
+    }
+
+
 }
