97e947bd139403878b569adf03ad66c6a9e9c2ab
rubik11111 <=>
2015-12-14 11:49:50 +0100

    Pierwsze wstępne robienia zarządzania operatorami.

---
+++ WroServer/WroServer/Models/OperatorModel/OperatorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WroServer.Models
+{
+    public class OperatorModel
+    {
+        public int Id { get; set; } 
+        public string Login { get; set; }   
+        public string Password { get; set; } 
+        public string Name { get; set; } 
+        public string Surname { get; set; } 
+        public string Contact { get; set; } 
+        public int ContactForm { get; set; } 
+        public int Role { get; set; } 
+    }
+}
